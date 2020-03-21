a2b3e781ffae2c21c4b7fa26168bec09547fd70f
Tim Weidner <timaa2k@gmail.com>
2020-03-21 12:08:08 +0100

    feat(nvim): add tmuxline

--- nvim/default.nix
+++ nvim/default.nix
@@ -23,6 +23,15 @@ let
       sha256 = "0n677r3snif7rq0z3l16ig9w9vb855ghf2p89n1l0z1s9y8619k0";
     };
   };
+  vim-tmuxline = vimUtils.buildVimPlugin {
+    name = "vim-tmuxline";
+    src = fetchFromGitHub {
+      owner = "edkolev";
+      repo = "tmuxline.vim";
+      rev = "7001ab359f2bc699b7000c297a0d9e9a897b70cf";
+      sha256 = "13d87zxpdzryal5dkircc0sm88mwwq7f5n4j3jn9f09fmg9siifb";
+    };
+  };
 in
 neovim.override {
   vimAlias = true;
@@ -33,8 +42,9 @@ neovim.override {
   configure.customRC = builtins.readFile ./vimrc;
   configure.packages.myVimPackages = with vimPlugins; {
     start = [
-      #lightline-vim
       vim-airline
+      vim-airline-themes
+      vim-tmuxline
       vim-colorschemes
       vim-colortuner
       papercolor-theme
