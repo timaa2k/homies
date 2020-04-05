a3be6ce73ee23b4e054afdeb7cb39780786c20ca
Tim Weidner <timaa2k@gmail.com>
2020-04-05 18:15:37 +0200

    feat(tmux): load nix tmux plugins

--- tmux/default.nix
+++ tmux/default.nix
@@ -1,11 +1,27 @@
-# Tmux with ./tmux.conf baked in
-{ tmux, writeText, symlinkJoin, makeWrapper }:
+{ lib, tmux, tmuxPlugins, symlinkJoin, makeWrapper, writeTextFile }:
+let
+  plugins = with tmuxPlugins; [
+    continuum
+    ctrlw
+    fzf-tmux-url
+    open
+    sensible
+    yank
+  ];
+
+  tmuxConf = writeTextFile {
+    name = "tmux-conf";
+    text = ( builtins.readFile ./tmux.conf ) + ''
+      ${lib.concatStrings (map (x: "run-shell ${x.rtp}\n") plugins)}
+    '';
+  };
+in
 symlinkJoin {
   name = "tmux";
-  buildInputs = [makeWrapper];
+  buildInputs = [ makeWrapper ];
   paths = [ tmux ];
   postBuild = ''
     wrapProgram "$out/bin/tmux" \
-    --add-flags "-f ${./tmux.conf}"
+    --add-flags "-f ${tmuxConf}"
   '';
 }
